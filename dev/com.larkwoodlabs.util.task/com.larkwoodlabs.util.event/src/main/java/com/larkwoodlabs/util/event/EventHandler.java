d69315fc82a69262b1ce1547a562d9a6fdf6d056
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2010-04-07 21:25:03 +0000

    Initial commit of project files.

---
+++ dev/com.larkwoodlabs.util.task/com.larkwoodlabs.util.event/src/main/java/com/larkwoodlabs/util/event/EventHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright © 2009-2010 Larkwood Labs Software.
+ *
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package com.larkwoodlabs.util.event;
+
+/**
+ * Interface that must be implemented by event handlers that can be attached to an {@link Event}
+ * @param <EventRecordType> The type of event object that will be passed to {@link #invoke(EventRecordType)}
+ * 
+ * @author Gregory Bumgardner
+ */
+public interface EventHandler<EventRecordType> {
+    
+    /**
+     * 
+     * @param record An object that describes the event.
+     * @return A Boolean value where:
+     * <li><code>true</code> indicates that the handler should be detached from the event.
+     * <li><code>false</code> indicates that the handler should remain attached to the event.
+     */
+    public boolean invoke(EventRecordType record);
+}
