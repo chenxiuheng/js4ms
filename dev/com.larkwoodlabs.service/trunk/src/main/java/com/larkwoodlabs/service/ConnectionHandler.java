e82a342999d009c8dd7f60faf3d670e6da7a24d0
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2010-11-08 18:20:29 +0000

    Modified to support new Service interface. Added reference to connection manager to allow handler to remove the connection when handler exits. Add Log member for log message formatting. Added/Modified comments.

--- dev/com.larkwoodlabs.service/trunk/src/main/java/com/larkwoodlabs/service/ConnectionHandler.java
+++ dev/com.larkwoodlabs.service/trunk/src/main/java/com/larkwoodlabs/service/ConnectionHandler.java
@@ -18,40 +18,35 @@ package com.larkwoodlabs.service;
 
 import java.io.EOFException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.SocketException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.larkwoodlabs.util.logging.Logging;
+import com.larkwoodlabs.util.logging.Log;
 
 
 /**
  * Base class for objects that manage the serialization of messages over a {@link Connection}.
  * This class simply flushes bytes from the connection input stream.
  * 
- * @author Gregory Bumgardner
+ * @author gbumgard@cisco.com
  */
 public class ConnectionHandler implements Runnable {
-
-    static public class Factory {
-        public ConnectionHandler construct(Service service, Connection connection) {
-            return new ConnectionHandler(service, connection);
-        }
-    }
 
     /*-- Static Variables ----------------------------------------------------*/
 
     public static final Logger logger = Logger.getLogger(ConnectionHandler.class.getName());
 
+
     /*-- Member Variables ----------------------------------------------------*/
 
-    protected final String ObjectId = Logging.identify(this);
+    protected final Log log = new Log(this);
 
     protected final Object lock = new Object();
 
-    protected Service service;
+    protected ConnectionManager manager;
     protected Connection connection;
+    protected Service service;
     
     protected boolean isRunning;
     
@@ -62,9 +57,15 @@ public class ConnectionHandler implements Runnable {
      * @param context
      * @param connection
      */
-    protected ConnectionHandler(final Service service, final Connection connection) {
-        this.service = service;
+    public ConnectionHandler(final ConnectionManager manager, final Connection connection, final Service service) {
+
+        if (logger.isLoggable(Level.FINER)) {
+            logger.finer(log.entry("ConnectionHandler", manager, connection, service));
+        }
+
+        this.manager = manager;
         this.connection = connection;
+        this.service = service;
     }
 
     /**
@@ -90,51 +91,49 @@ public class ConnectionHandler implements Runnable {
     }
 
     /**
+     * 
+     * @return
+     */
+    public ConnectionManager getConnectionManager() {
+        return this.manager;
+    }
+
+    /**
      * Continuously receives and forwards incoming bytes.
      */
     @Override
     public void run() {
 
         if (logger.isLoggable(Level.FINER)) {
-            logger.finer(Logging.entering(ObjectId, "ConnectionHandler.run"));
+            logger.finer(log.entry("run"));
         }
 
         try {
             while (!Thread.currentThread().isInterrupted()) {
-                // Read and process one message from the input stream
-                processMessage();
+                // Read and process incoming bytes
+                this.service.service(connection);
             }
         }
         catch (EOFException e) {
             // Connection was closed by peer or the input was shutdown
-            if (logger.isLoggable(Level.FINE)) {
-                logger.fine(ObjectId + " connection handler exiting for " + e.getClass().getName() + ": " + e.getMessage());
-            }
+            logger.fine(log.msg("connection handler exiting for " + e.getClass().getName() + ": " + e.getMessage()));
         }
         catch (SocketException e) {
             // The connection socket was closed by another thread while this thread was waiting on I/O.
-            if (logger.isLoggable(Level.FINE)) {
-                logger.fine(ObjectId + " connection handler exiting for " + e.getClass().getName() + ": " + e.getMessage());
-            }
+            logger.fine(log.msg("connection handler exiting for " + e.getClass().getName() + ": " + e.getMessage()));
         }
         catch (IOException e) {
             // IO exception occurred - most likely while attempting to send a message or data over a closed connection
-            if (logger.isLoggable(Level.FINE)) {
-                logger.warning(ObjectId + " connection handler aborted by " + e.getClass().getName() + ":" + e.getMessage());
-            }
+            logger.warning(log.msg("connection handler aborted by " + e.getClass().getName() + ":" + e.getMessage()));
         }
         catch (InterruptedException e) {
-            if (logger.isLoggable(Level.FINE)) {
-                logger.fine(ObjectId + " connection handler thread was interrupted");
-                // Continue on to attempt to close the connection but set the flag to interrupt any subsequent waits
-                Thread.currentThread().interrupt();
-            }
+            logger.fine(log.msg("connection handler thread was interrupted"));
+            // Continue on to attempt to close the connection but set the flag to interrupt any subsequent waits
+            Thread.currentThread().interrupt();
         }
         catch (Exception e) {
             // An unexpected exception occurred
-            if (logger.isLoggable(Level.FINE)) {
-                logger.warning(ObjectId + " connection handler aborted by " + e.getClass().getName() + ":" + e.getMessage());
-            }
+            logger.warning(log.msg("connection handler aborted by " + e.getClass().getName() + ":" + e.getMessage()));
             e.printStackTrace();
         }
 
@@ -142,44 +141,12 @@ public class ConnectionHandler implements Runnable {
             this.connection.close();
         }
         catch (IOException e) {
-            if (logger.isLoggable(Level.FINE)) {
-                logger.fine(ObjectId +
-                            " connection handler cannot close connection - " +
-                            e.getClass().getName() + ":" + e.getMessage());
-            }
+            logger.fine(log.msg("connection handler cannot close connection - " + e.getClass().getName() + ":" + e.getMessage()));
             e.printStackTrace();
         }
 
-        this.service.removeConnection(connection);
+        this.manager.removeConnection(connection);
 
     }
 
-    /**
-     * Reads and dispatches messages and interleaved packets from the connection InputStream.
-     * Typically overridden in derived classes. Default implementation simply flushes bytes
-     * from the stream.
-     * @throws Exception
-     */
-    public void processMessage() throws EOFException,
-                                        SocketException,
-                                        IOException,
-                                        InterruptedException {
-
-        if (logger.isLoggable(Level.FINER)) {
-            logger.finer(Logging.entering(ObjectId, "ConnectionHandler.processMessage"));
-        }
-
-        InputStream inputStream = this.connection.getInputStream();
-
-        // Get first character in message 
-        // Throws SocketException if the socket is closed by 
-        // another thread while waiting in this call
-        int c = inputStream.read();
-
-        if (c == -1) {
-            // Peer stopped sending data or input was shutdown
-            throw new EOFException("connection stream returned EOF");
-        }
-
-    }
 }
