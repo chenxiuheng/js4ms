6d3e8ee7289dacb9e565c78ceb8243efcea3c433
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2010-11-08 19:10:01 +0000

    Initial Commit.

---
+++ dev/com.larkwoodlabs.service.protocol.http/trunk/src/src/main/java/com/larkwoodlabs/service/protocol/http/HttpMethods.java
@@ -0,0 +1,14 @@
+package com.larkwoodlabs.service.protocol.http;
+
+import com.larkwoodlabs.service.protocol.text.message.Method;
+
+public interface HttpMethods {
+    public static final Method GET = new Method("GET");
+    public static final Method HEAD = new Method("HEAD");
+    public static final Method POST = new Method("POST");
+    public static final Method PUT = new Method("PUT");
+    public static final Method DELETE = new Method("DELETE");
+    public static final Method TRACE = new Method("TRACE");
+    public static final Method CONNECT = new Method("CONNECT");
+    public static final Method PATCH = new Method("PATCH");
+}
---
+++ dev/com.larkwoodlabs.service.protocol.http/trunk/src/src/main/java/com/larkwoodlabs/service/protocol/http/HttpStatusCodes.java
@@ -0,0 +1,7 @@
+package com.larkwoodlabs.service.protocol.http;
+
+import com.larkwoodlabs.service.protocol.text.StatusCodes;
+
+public interface HttpStatusCodes extends StatusCodes {
+
+}
