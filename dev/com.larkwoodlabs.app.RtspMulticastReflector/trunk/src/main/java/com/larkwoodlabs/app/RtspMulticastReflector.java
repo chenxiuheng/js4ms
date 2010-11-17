e1dc09aa17bb8f507b7f54a69e61276c76fc6d6c
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2010-11-17 20:06:41 +0000

    Added a new project that contains an implementation of an RTSP multicast reflector based on the new service classes. The reflector uses SDP descriptions of multicast media streams to construct an AMT tunnel that receives multicast packets and reflects them through unicast RTSP channels for delivery to non-multicast enabled RTSP clients. The reflector also provides an HTTP-based interface for log streaming and configuration.

---
+++ dev/com.larkwoodlabs.app.RtspMulticastReflector/trunk/src/main/java/com/larkwoodlabs/app/RtspMulticastReflector.java
@@ -0,0 +1,119 @@
+package com.larkwoodlabs.app;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import com.larkwoodlabs.service.Connection;
+import com.larkwoodlabs.service.ConnectionHandler;
+import com.larkwoodlabs.service.ConnectionHandlerFactory;
+import com.larkwoodlabs.service.ConnectionManager;
+import com.larkwoodlabs.service.Server;
+import com.larkwoodlabs.service.ServerFactory;
+import com.larkwoodlabs.service.Service;
+import com.larkwoodlabs.service.protocol.http.server.handlers.LoggingConfigurationHandler;
+import com.larkwoodlabs.service.protocol.http.server.handlers.LoggingOutputHandler;
+import com.larkwoodlabs.service.protocol.rtsp.RtspStatusCodes;
+import com.larkwoodlabs.service.protocol.rtsp.presentation.PresentationUriPathResolver;
+import com.larkwoodlabs.service.protocol.rtsp.reflector.MulticastReflectorFactory;
+import com.larkwoodlabs.service.protocol.rtsp.server.RtspService;
+import com.larkwoodlabs.service.protocol.text.entity.StringEntity;
+import com.larkwoodlabs.service.protocol.text.handler.ResponseHandlerList;
+import com.larkwoodlabs.service.protocol.text.message.Request;
+import com.larkwoodlabs.service.protocol.text.message.Response;
+import com.larkwoodlabs.service.protocol.text.handler.TransactionDispatcher;
+import com.larkwoodlabs.service.protocol.text.handler.TransactionHandler;
+import com.larkwoodlabs.service.protocol.text.handler.TransactionProtocolResolver;
+import com.larkwoodlabs.service.protocol.text.handler.TransactionUriPathResolver;
+import com.larkwoodlabs.service.protocol.text.server.handlers.AddServerHeader;
+import com.larkwoodlabs.util.logging.Logging;
+
+public class RtspMulticastReflector {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        try {
+            Logging.configureLogging();
+        }
+        catch (IOException e) {
+        }
+
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name", "");
+
+        try {
+            Server.runServer(System.getProperties(), new ServerFactory() {
+
+                @Override
+                public Server construct(Properties properties) {
+
+                    PresentationUriPathResolver reflectorResolver = new PresentationUriPathResolver();
+                    reflectorResolver.put("/reflect", new MulticastReflectorFactory());
+
+                    final RtspService service = new RtspService(reflectorResolver);
+
+                    ResponseHandlerList decorators = service.getResponseHandlers();
+                    decorators.addHandler(new AddServerHeader("RTSP Multicast Reflector"));
+
+                    final Server server = new Server(properties,
+                                                     service,
+                                                     new ConnectionHandlerFactory() {
+                                                         @Override
+                                                         public ConnectionHandler construct(ConnectionManager manager,
+                                                                                            Connection connection,
+                                                                                            Service service) {
+                                                             return new ConnectionHandler(manager, connection, service);
+                                                         }
+                                                     });
+
+                    // Add HTTP resource that can be used to shutdown the server
+
+                    TransactionUriPathResolver adminResolver = new TransactionUriPathResolver();
+                    adminResolver.put("/shutdown", new TransactionHandler() {
+                        @Override
+                        public boolean handleTransaction(Request request, Response response) throws IOException {
+                            try {
+                                response.setStatus(RtspStatusCodes.OK);
+                                response.setEntity(new StringEntity("stopping RTSP Multicast reflector..."));
+                                response.send();
+                                server.stop();
+                            }
+                            catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                response.setStatus(RtspStatusCodes.ServiceUnavailable);
+                            }
+                            return true;
+                        }
+                    });
+
+                    adminResolver.put("/log", new LoggingOutputHandler());
+
+                    adminResolver.put("/loggers", new LoggingConfigurationHandler());
+
+                    adminResolver.put("/*", new TransactionHandler() {
+                        @Override
+                        public boolean handleTransaction(Request request, Response response) throws IOException {
+                            response.setStatus(RtspStatusCodes.Forbidden);
+                            response.setEntity(new StringEntity(RtspStatusCodes.Forbidden.toString()));
+                            response.send();
+                            return true;
+                        }
+                    });
+
+                    TransactionProtocolResolver protocolResolver = new TransactionProtocolResolver();
+                    protocolResolver.put("HTTP", adminResolver);
+
+                    service.getTransactionHandlers().addHandler(new TransactionDispatcher(protocolResolver));
+
+                    return server;
+                }
+
+            });
+        }
+        catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
