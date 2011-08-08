9ff518a018f07c45a3c5d4f80df5212486a01647
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2011-08-08 19:11:17 +0000

    Added new AmtInterface subclasses for IPv4 and IPv6.

---
+++ dev/com.larkwoodlabs.net.amt/trunk/src/main/java/com/larkwoodlabs/net/amt/AmtIPv6Interface.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright © 2009-2010 Larkwood Labs Software.
+ *
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package com.larkwoodlabs.net.amt;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.larkwoodlabs.channels.OutputChannelTransform;
+import com.larkwoodlabs.net.ip.IPPacket;
+import com.larkwoodlabs.util.logging.Logging;
+
+/**
+ * Manages an AMT tunnel end-point.
+ * The {@link AmtGateway} constructs a separate AmtInterface for each unique 
+ * AMT relay or AMT gateway peer acting as a remote AMT tunnel end-point.
+ * An AmtInterface provides functions for joining, leaving and receiving packets
+ * for any number of multicast groups. The AmtInterface tracks local group
+ * membership state and handles the exchange of IGMP/MLD messages used
+ * to query or update that state.
+ *
+ * @author gbumgard
+ */
+public final class AmtIPv6Interface extends AmtInterface {
+
+    /*-- Static Variables ----------------------------------------------------*/
+
+    public static final Logger logger = Logger.getLogger(AmtIPv6Interface.class.getName());
+
+
+    /*-- Member Variables ---------------------------------------------------*/
+
+    final String ObjectId = Logging.identify(this);
+
+
+    /*-- Member Functions ---------------------------------------------------*/
+    
+    /**
+     * 
+     * @param gateway
+     * @param relayDiscoveryAddress
+     * @throws IOException
+     */
+    public AmtIPv6Interface(final AmtGateway gateway, final InetAddress relayDiscoveryAddress) throws IOException {
+        super(gateway, relayDiscoveryAddress);
+
+        if (logger.isLoggable(Level.FINER)) {
+            logger.finer(Logging.entering(ObjectId, "AmtIPv4Interface.AmtIPv4Interface", gateway, Logging.address(relayDiscoveryAddress)));
+        }
+
+        // Connect report channel of interface membership manager to tunnel endpoint
+        this.interfaceManager.setOutgoingReportChannel(
+                new OutputChannelTransform<MembershipReport,IPPacket>(
+                        this.tunnelEndpoint.getOutgoingUpdateChannel(),
+                        new IPv4MembershipReportTransform()));
+        
+        // Connect query channel of tunnel endpoint to interface membership manager
+        this.tunnelEndpoint.setIncomingQueryChannel(
+                new OutputChannelTransform<IPPacket, MembershipQuery>(
+                        this.interfaceManager.getIncomingQueryChannel(),
+                        new IPv6MembershipQueryTransform(this.tunnelEndpoint)));
+        
+
+        this.tunnelEndpoint.start();
+    }
+
+}
