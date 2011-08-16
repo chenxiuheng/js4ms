ef021861f953d4eaaafea05cbef65d15846ea0e6
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2011-08-16 19:39:41 +0000

    Made DateHeader class a subclass of FormattedMessageHeader. Added DateHeader copy constructor.

--- dev/com.larkwoodlabs.service.protocol.text/trunk/src/main/java/com/larkwoodlabs/service/protocol/text/headers/DateHeader.java
+++ dev/com.larkwoodlabs.service.protocol.text/trunk/src/main/java/com/larkwoodlabs/service/protocol/text/headers/DateHeader.java
@@ -2,14 +2,50 @@ package com.larkwoodlabs.service.protocol.text.headers;
 
 import java.util.Date;
 
-import com.larkwoodlabs.service.protocol.text.message.Header;
+import com.larkwoodlabs.common.exceptions.ParseException;
+import com.larkwoodlabs.service.protocol.text.message.FormattedMessageHeader;
+import com.larkwoodlabs.service.protocol.text.message.MessageHeader;
 import com.larkwoodlabs.service.protocol.text.util.DateUtil;
 
 
-public class DateHeader extends Header {
+public class DateHeader extends FormattedMessageHeader {
+
+    private Date date;
 
     public DateHeader(final String name, final Date value) {
-        super(name,DateUtil.DATE_FORMAT_RFC_1123.format(value));
+        super(name);
+        this.date = value;
+    }
+
+    public DateHeader(final DateHeader header) {
+        super(header);
+        this.date = header.date;
+    }
+
+    @Override
+    public void appendHeader(MessageHeader header) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void parse(String value) throws ParseException {
+        try {
+            this.date = DateUtil.toDate(value);
+        }
+        catch (java.text.ParseException e) {
+            throw new ParseException(e);
+        }
+    }
+
+    @Override
+    protected String format() {
+        return DateUtil.DATE_FORMAT_RFC_1123.format(this.date);
+    }
+
+    @Override
+    public Object clone() {
+        return new DateHeader(this);
     }
 
 }
