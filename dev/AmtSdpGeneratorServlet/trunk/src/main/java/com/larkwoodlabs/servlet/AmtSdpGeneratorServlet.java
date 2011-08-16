188562e9c740dcaf1e9e302c3841ed7e8d233109
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2011-08-16 18:41:21 +0000

    Changed to use address that appears in SDP origin record as the source address in the source-filter attribute record.

--- dev/AmtSdpGeneratorServlet/trunk/src/main/java/com/larkwoodlabs/servlet/AmtSdpGeneratorServlet.java
+++ dev/AmtSdpGeneratorServlet/trunk/src/main/java/com/larkwoodlabs/servlet/AmtSdpGeneratorServlet.java
@@ -1,5 +1,9 @@
 package com.larkwoodlabs.servlet;
+import gov.nist.core.Host;
 import gov.nist.javax.sdp.fields.AttributeField;
+import gov.nist.javax.sdp.fields.ConnectionAddress;
+import gov.nist.javax.sdp.fields.ConnectionField;
+import gov.nist.javax.sdp.fields.OriginField;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,10 +11,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
 import java.net.HttpURLConnection;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Vector;
 
+import javax.sdp.Connection;
+import javax.sdp.MediaDescription;
+import javax.sdp.Origin;
 import javax.sdp.SdpException;
 import javax.sdp.SdpFactory;
 import javax.sdp.SdpParseException;
@@ -23,7 +32,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class AmtSdpGeneratorServlet extends HttpServlet {
 
-    static final String RELAY_ATTRIBUTE_NAME = "x-amt-relay-discovery-address";
+    static final String SOURCE_FILTER_ATTRIBUTE_NAME = "source-filter";
+    static final String RELAY_DISCOVERY_ADDRESS_ATTRIBUTE_NAME = "x-amt-relay-discovery-address";
     static final String SDP_QUERY_PARAMETER = "src_sdp";
     static final String RELAY_QUERY_PARAMETER = "src_relay";
 
@@ -152,17 +162,85 @@ public class AmtSdpGeneratorServlet extends HttpServlet {
         }
         
         try {
-            if (sessionDescription.getAttribute(RELAY_ATTRIBUTE_NAME) != null) {
+            // Add or update the relay discovery attribute record
+            if (sessionDescription.getAttribute(RELAY_DISCOVERY_ADDRESS_ATTRIBUTE_NAME) != null) {
                 // Change attribute value
-                sessionDescription.setAttribute(RELAY_ATTRIBUTE_NAME, relayAddress);
+                sessionDescription.setAttribute(RELAY_DISCOVERY_ADDRESS_ATTRIBUTE_NAME, relayAddress);
             }
             else {
                 // Add attribute
-                AttributeField xAmtRelayAnycast = new AttributeField();
-                xAmtRelayAnycast.setName(RELAY_ATTRIBUTE_NAME);
-                xAmtRelayAnycast.setValue(relayAddress);
+                AttributeField xAmtRelayDiscoveryAddress = new AttributeField();
+                xAmtRelayDiscoveryAddress.setName(RELAY_DISCOVERY_ADDRESS_ATTRIBUTE_NAME);
+                xAmtRelayDiscoveryAddress.setValue(relayAddress);
                 Vector attributes = sessionDescription.getAttributes(false);
-                attributes.add(xAmtRelayAnycast);
+                attributes.add(xAmtRelayDiscoveryAddress);
+                sessionDescription.setAttributes(attributes);
+            }
+
+            if (sessionDescription.getAttribute(SOURCE_FILTER_ATTRIBUTE_NAME) == null) {
+
+                // Verify that a session or media-level connection record specifies a multicast destination address
+                Connection connection = sessionDescription.getConnection();
+                if (connection == null) {
+                    Vector<?> descriptions = sessionDescription.getMediaDescriptions(false);
+                    if (descriptions != null) {
+                        for (int i=0; i<descriptions.size(); i++)
+                        {
+                            MediaDescription mediaDescription = (MediaDescription)descriptions.get(i);
+
+                            connection = mediaDescription.getConnection();
+                            if (connection != null) {
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (connection == null) {
+                    response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "SDP file does not specify a connection address");
+                    return;
+                }
+
+                ConnectionField connectionField = (ConnectionField)connection;
+                ConnectionAddress connectionAddress = connectionField.getConnectionAddress();
+
+                Host host = connectionAddress.getAddress();
+                InetAddress groupAddress;
+                try {
+                    groupAddress = host.getInetAddress();
+                }
+                catch (UnknownHostException e) {
+                    response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "SDP file does not specify a resolvable connection address");
+                    return;
+                }
+
+                if (!groupAddress.isMulticastAddress()) {
+                    response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "SDP file does not specify a multicast connection address");
+                    return;
+                }
+
+                // Add or update the relay discovery attribute record
+                Origin origin = sessionDescription.getOrigin();
+                if (origin == null) {
+                    response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "SDP file does not specify an origin");
+                    return;
+                }
+
+                OriginField originField = (OriginField)origin;
+                InetAddress sourceAddress = InetAddress.getByName(originField.getAddress());
+
+                if (!groupAddress.getClass().equals(sourceAddress.getClass())) {
+                    response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "the IP address types of the source and group address do not match");
+                    return;
+                }
+
+                // Add source filter attribute that applies to all groups
+                String filterDesc = "incl IN " + ((sourceAddress instanceof Inet4Address) ? "IP4" : "IP6") + " * " + sourceAddress.getHostAddress();
+                AttributeField sourceFilter = new AttributeField();
+                sourceFilter.setName(SOURCE_FILTER_ATTRIBUTE_NAME);
+                sourceFilter.setValue(filterDesc);
+                Vector attributes = sessionDescription.getAttributes(false);
+                attributes.add(sourceFilter);
                 sessionDescription.setAttributes(attributes);
             }
         } catch (SdpException e) {
