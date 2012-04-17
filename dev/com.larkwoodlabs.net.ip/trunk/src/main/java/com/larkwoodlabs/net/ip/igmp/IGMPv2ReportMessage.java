bbe547af74cfa349fd0c5f2ab0820d52c6f704c5
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-04-17 23:03:07 +0000

    Added new (missing) IGMPv2 Report Message class.

---
+++ dev/com.larkwoodlabs.net.ip/trunk/src/main/java/com/larkwoodlabs/net/ip/igmp/IGMPv2ReportMessage.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright © 2009-2010 Larkwood Labs Software.
+ *
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package com.larkwoodlabs.net.ip.igmp;
+
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+
+import com.larkwoodlabs.common.exceptions.ParseException;
+import com.larkwoodlabs.net.ip.IPMessage;
+import com.larkwoodlabs.net.ip.IPPacket;
+import com.larkwoodlabs.net.ip.ipv4.IPv4Packet;
+import com.larkwoodlabs.util.buffer.parser.MissingParserException;
+import com.larkwoodlabs.util.logging.Logging;
+
+/**
+ * An IGMPv2 Membership Report Message. [See <a
+ * href="http://www.ietf.org/rfc/rfc2236.txt">RFC-2236</a>]
+ * 
+ * <pre>
+ *      0                   1                   2                   3
+ *      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *     |  Type = 0x16  | Max Resp Code |           Checksum            |
+ *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *     |                         Group Address                         |
+ *     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * 
+ * Type
+ * 
+ *    0x16 = Membership Report
+ * 
+ * Max Response Code
+ * 
+ *    The Max Response Code field is meaningful only in Membership Query
+ *    messages.
+ *
+ * Checksum
+ * 
+ *    The checksum is the 16-bit one's complement of the one's complement
+ *    sum of the whole IGMP message (the entire IP payload).  For computing
+ *    the checksum, the checksum field is set to zero.  When transmitting
+ *    packets, the checksum MUST be computed and inserted into this field.
+ *    When receiving packets, the checksum MUST be verified before
+ *    processing a packet.
+ * 
+ * Group Address
+ * 
+ *    In a Membership Report message, the group address field holds the 
+ *    IP multicast group address of the group being or currently joined.
+ * </pre>
+ * 
+ * @author Gregory Bumgardner
+ * 
+ */
+public final class IGMPv2ReportMessage extends IGMPGroupMessage {
+
+    /*-- Inner Classes ------------------------------------------------------*/
+
+    /**
+     * 
+     */
+    public static final class Parser implements IGMPMessage.ParserType {
+
+        @Override
+        public IGMPMessage parse(final ByteBuffer buffer) throws ParseException {
+            return new IGMPv2ReportMessage(buffer);
+        }
+
+        @Override
+        public boolean verifyChecksum(final ByteBuffer buffer) throws MissingParserException, ParseException {
+            return IGMPv2ReportMessage.verifyChecksum(buffer);
+        }
+
+        @Override
+        public Object getKey() {
+            return MESSAGE_TYPE;
+        }
+
+    }
+
+
+    /*-- Static Variables ---------------------------------------------------*/
+
+    /** */
+    public static final byte MESSAGE_TYPE = 0x16;
+
+    /** */
+    public static final int BASE_MESSAGE_LENGTH = 8;
+
+
+    /*-- Static Functions ---------------------------------------------------*/
+
+    /**
+     * 
+     * @return
+     */
+    public static IGMPMessage.Parser getIGMPMessageParser() {
+        return getIGMPMessageParser(new IGMPv2ReportMessage.Parser());
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static IPMessage.Parser getIPMessageParser() {
+        return getIPMessageParser(new IGMPv2ReportMessage.Parser());
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static IPv4Packet.Parser getIPv4PacketParser() {
+        return getIPv4PacketParser(new IGMPv2ReportMessage.Parser());
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static IPPacket.BufferParser getIPPacketParser() {
+        return getIPPacketParser(new IGMPv2ReportMessage.Parser());
+    }
+
+    /**
+     * Verifies the IGMP message checksum. Called by the parser prior to constructing the packet.
+     * @param buffer - the buffer containing the IGMP message.
+     */
+    public static boolean verifyChecksum(final ByteBuffer buffer) {
+        return Checksum.get(buffer) == IGMPMessage.calculateChecksum(buffer, BASE_MESSAGE_LENGTH);
+    }
+
+    /**
+     * Writes the IGMP message checksum into a buffer containing an IGMP message.
+     * @param buffer
+     */
+    public static void setChecksum(final ByteBuffer buffer) {
+        Checksum.set(buffer, IGMPMessage.calculateChecksum(buffer, BASE_MESSAGE_LENGTH));
+    }
+
+    
+    /*-- Member Functions ---------------------------------------------------*/
+
+    /**
+     * 
+     * @param groupAddress
+     */
+    public IGMPv2ReportMessage(final byte[] groupAddress) {
+        super(BASE_MESSAGE_LENGTH,MESSAGE_TYPE,(byte)0,groupAddress);
+        
+        if (logger.isLoggable(Level.FINER)) {
+            logger.finer(Logging.entering(ObjectId, "IGMPv2ReportMessage.IGMPv2ReportMessage", Logging.address(groupAddress)));
+        }
+    }
+
+    /**
+     * 
+     * @param buffer
+     * @throws ParseException
+     */
+    public IGMPv2ReportMessage(final ByteBuffer buffer) throws ParseException {
+        super(consume(buffer, BASE_MESSAGE_LENGTH));
+        if (logger.isLoggable(Level.FINER)) {
+            logger.finer(Logging.entering(ObjectId, "IGMPv2ReportMessage.IGMPv2ReportMessage", buffer));
+        }
+    }
+
+    @Override
+    public void writeChecksum(final ByteBuffer buffer,
+                              final byte[] sourceAddress,
+                              final byte[] destinationAddress) {
+        
+        if (logger.isLoggable(Level.FINER)) {
+            logger.finer(Logging.entering(ObjectId, "IGMPv2ReportMessage.writeChecksum", buffer, Logging.address(sourceAddress), Logging.address(destinationAddress)));
+        }
+
+        IGMPv2ReportMessage.setChecksum(buffer);
+    }
+
+    @Override
+    public int getMessageLength() {
+        return BASE_MESSAGE_LENGTH;
+    }
+
+    @Override
+    public byte getType() {
+        return MESSAGE_TYPE;
+    }
+
+}
