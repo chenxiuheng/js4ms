d0717c50f3b740835f77a657ee2a82f72a6005f6
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-04-19 20:49:06 +0000

    Initial commit.

---
+++ dev/com.larkwoodlabs.util.logging.android/trunk/src/main/java/com/larkwoodlabs/util/logging/android/LogCatFormatter.java
@@ -0,0 +1,27 @@
+package com.larkwoodlabs.util.logging.android;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class LogCatFormatter extends Formatter {
+    
+    // private static final MessageFormat messageFormat = new MessageFormat("{0,date,HH:mm:ss.SSS} {1,number,00000000} {2} {3} {4} {5}\n");
+    // private static final MessageFormat messageFormat = new MessageFormat("{0} {1}\n");
+    
+    // private long startTime = System.currentTimeMillis();
+
+    public LogCatFormatter() {
+        super();
+    }
+    
+    @Override
+    public String format(LogRecord record) {
+
+        //Object[] arguments = new Object[7];
+        // Object[] arguments = new Object[2];
+        // arguments[0] = record.getMessage();
+
+        //return messageFormat.format(arguments);
+        return record.getMessage();
+    }   
+}
---
+++ dev/com.larkwoodlabs.util.logging.android/trunk/src/main/java/com/larkwoodlabs/util/logging/android/LogCatHandler.java
@@ -0,0 +1,41 @@
+package com.larkwoodlabs.util.logging.android;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+import android.util.Log;
+
+public class LogCatHandler extends Handler {
+
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void flush() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+
+        if (!isLoggable(record)) return;
+        
+        Level loggerLevel = record.getLevel();
+
+        int logcatLevel = Log.INFO;
+        if (loggerLevel == Level.SEVERE) logcatLevel = Log.ERROR;
+        else if (loggerLevel == Level.WARNING) logcatLevel = Log.WARN;
+        else if (loggerLevel == Level.INFO) logcatLevel = Log.INFO;
+        else if (loggerLevel == Level.FINE) logcatLevel = Log.DEBUG;
+        else if (loggerLevel == Level.FINER) logcatLevel = Log.VERBOSE;
+        else if (loggerLevel == Level.FINEST) logcatLevel = Log.VERBOSE;
+
+        // Output the formatted data to the file
+        Log.println(logcatLevel, record.getLoggerName(), getFormatter().format(record));
+    }
+}
