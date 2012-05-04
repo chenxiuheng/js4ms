3d8d1ae0febe009793d80ff69fac3601a0e093eb
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-05-04 18:50:37 +0000

    Added license text.

--- dev/com.larkwoodlabs.net.amt.gateway/trunk/src/main/java/com/larkwoodlabs/net/amt/gateway/AmtDatagramSource.java
+++ dev/com.larkwoodlabs.net.amt.gateway/trunk/src/main/java/com/larkwoodlabs/net/amt/gateway/AmtDatagramSource.java
@@ -1,3 +1,23 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * File: AmtDatagramSource.java (com.larkwoodlabs.net.amt.gateway)
+ * 
+ * Copyright © 2009-2012 Cisco Systems, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.larkwoodlabs.net.amt.gateway;
 
 import java.io.IOException;
@@ -17,7 +37,7 @@ import com.larkwoodlabs.util.logging.Log;
  * and port and forwards those packets to an {@link OutputChannel<UdpDatagram>} instance.
  * Forwarding can be enabled or disabled using the {@link #start()} and {@link #stop()} methods.
  * 
- * @author gbumgard@cisco.com
+ * @author Greg Bumgardner (gbumgard)
  */
 public class AmtDatagramSource extends MessageSource<UdpDatagram> {
 
