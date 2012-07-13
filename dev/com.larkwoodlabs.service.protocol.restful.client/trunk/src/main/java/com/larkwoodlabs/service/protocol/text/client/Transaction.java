0d4490f412a6c7885cc346ef4f65f8e0ca6748ba
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-07-13 19:11:33 +0000

    Deleted unused class that will be contained in the super-package.

--- dev/com.larkwoodlabs.service.protocol.restful.client/trunk/src/main/java/com/larkwoodlabs/service/protocol/text/client/Transaction.java
+++
@@ -1,8 +0,0 @@
-package com.larkwoodlabs.service.protocol.text.client;
-
-import com.larkwoodlabs.service.protocol.restful.handler.TransactionHandler;
-
-public class Transaction {
-    @SuppressWarnings("unused")
-    private TransactionHandler handler;
-}
