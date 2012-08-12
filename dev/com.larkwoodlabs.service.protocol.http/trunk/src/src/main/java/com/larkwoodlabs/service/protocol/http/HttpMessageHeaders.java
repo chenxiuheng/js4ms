c58fa67eb418ec67517261eb2204ba5893d8c626
gregb@larkwoodlabs.com <gregb@larkwoodlabs.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-08-12 19:22:59 +0000

    Renamed "..protocol.restful.." packages to "..protocol.rest.."

--- dev/com.larkwoodlabs.service.protocol.http/trunk/src/src/main/java/com/larkwoodlabs/service/protocol/http/HttpMessageHeaders.java
+++ dev/com.larkwoodlabs.service.protocol.http/trunk/src/src/main/java/com/larkwoodlabs/service/protocol/http/HttpMessageHeaders.java
@@ -1,6 +1,6 @@
 package com.larkwoodlabs.service.protocol.http;
 
-import com.larkwoodlabs.service.protocol.restful.MessageHeaders;
+import com.larkwoodlabs.service.protocol.rest.MessageHeaders;
 
 public interface HttpMessageHeaders extends MessageHeaders {
 
