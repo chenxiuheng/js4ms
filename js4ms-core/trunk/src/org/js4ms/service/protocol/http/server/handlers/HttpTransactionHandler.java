591fe42d3c19e6846399604268eb211b6ca95472
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-08-17 06:17:36 +0000

    Moved org.js4ms.service.protocol.http.server.handlers to js4ms-core project.

---
+++ js4ms-core/trunk/src/org/js4ms/service/protocol/http/server/handlers/HttpTransactionHandler.java
@@ -0,0 +1,168 @@
+package org.js4ms.service.protocol.http.server.handlers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Map;
+
+import org.js4ms.service.protocol.http.HttpMethods;
+import org.js4ms.service.protocol.http.HttpStatusCodes;
+import org.js4ms.service.protocol.rest.entity.CodecManager;
+import org.js4ms.service.protocol.rest.entity.Entity;
+import org.js4ms.service.protocol.rest.entity.MediaType;
+import org.js4ms.service.protocol.rest.entity.StringEntity;
+import org.js4ms.service.protocol.rest.handler.TransactionHandler;
+import org.js4ms.service.protocol.rest.message.Method;
+import org.js4ms.service.protocol.rest.message.Request;
+import org.js4ms.service.protocol.rest.message.Response;
+
+
+public class HttpTransactionHandler
+                implements TransactionHandler {
+
+    @Override
+    public boolean handleTransaction(final Request request, final Response response) throws IOException {
+        Method method = request.getRequestLine().getMethod();
+        if (method == HttpMethods.GET) {
+            return doGet(request, response);
+        }
+        else if (method == HttpMethods.POST) {
+            return doPost(request, response);
+        }
+        else if (method == HttpMethods.PUT) {
+            return doPut(request, response);
+        }
+        else if (method == HttpMethods.PATCH) {
+            return doPatch(request, response);
+        }
+        else if (method == HttpMethods.DELETE) {
+            return doDelete(request, response);
+        }
+        else if (method == HttpMethods.TRACE) {
+            return doTrace(request, response);
+        }
+        else if (method == HttpMethods.CONNECT) {
+            return doConnect(request, response);
+        }
+        else {
+            return doDefaultResponse(request, response);
+        }
+    }
+
+    protected boolean doGet(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doPost(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doPut(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doPatch(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doDelete(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doTrace(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    protected boolean doConnect(final Request request, final Response response) throws IOException {
+        return doDefaultResponse(request, response);
+    }
+
+    private boolean doDefaultResponse(final Request request, final Response response) throws IOException {
+        response.setStatus(HttpStatusCodes.MethodNotAllowed);
+        response.setEntity(new StringEntity(HttpStatusCodes.MethodNotAllowed.toString()));
+        response.send();
+        return true;
+    }
+
+    static void parseQueryParameters(final Request request, Map<String, String> map) {
+        // Parse query string
+        parseParameterString(request.getRequestLine().getUri().getQuery(), map);
+    }
+
+    static boolean parseFormParameters(final Request request, final Response response, Map<String, String> map) throws IOException {
+        // Parse query string
+        if (request.containsHeader(Entity.CONTENT_TYPE)) {
+            MediaType mediaType = MediaType.parse(request.getHeader(Entity.CONTENT_TYPE).getValue());
+            if (mediaType.equals(MediaType.MULTIPART_FORM_DATA_TYPE)) {
+                if (!mediaType.containsParameter("boundary")) {
+                    response.setStatus(HttpStatusCodes.BadRequest);
+                    response.setEntity(new StringEntity(HttpStatusCodes.BadRequest.toString()
+                                                        + " - multipart content type missing boundary parameter"));
+                    response.send();
+                    return true;
+                }
+
+                return parseMultipartFormParameters((new BufferedReader(
+                                                                        new InputStreamReader(
+                                                                                              request.getEntity()
+                                                                                                              .getContent(CodecManager.getManager()
+                                                                                                                                          .getCodec("*"))))
+                                                                     .readLine()),
+                                                    mediaType.getParameterValue("boundary").trim().replaceAll("^\"|\"$", ""),
+                                                    map);
+            }
+            else if (mediaType.equals(MediaType.APPLICATION_X_WWW_FORM_URLENCODED_TYPE)) {
+                parseParameterString(new BufferedReader(
+                                                        new InputStreamReader(
+                                                                              request.getEntity()
+                                                                                              .getContent(CodecManager.getManager()
+                                                                                                                          .getCodec("*"))))
+                                                     .readLine(), map);
+            }
+            else {
+                response.setStatus(HttpStatusCodes.UnsupportedMediaType);
+                response.setEntity(new StringEntity(HttpStatusCodes.UnsupportedMediaType.toString()));
+                response.send();
+                return true;
+            }
+        }
+        parseParameterString(request.getEntity().toString(), map);
+        return false;
+    }
+
+    static boolean parseMultipartFormParameters(final String entity, final String boundary, final Map<String, String> map) {
+        // TODO
+        /*
+        String parts[] = entity.split("^--"+boundary+"(--)$");
+        for (String part : parts) {
+        }
+        **/
+        return false;
+    }
+
+    static void parseParameterString(final String parameterString, final Map<String, String> map) {
+        if (parameterString != null && parameterString.length() > 0) {
+            String parameters[] = parameterString.split("[&;]");
+            for (String parameter : parameters) {
+                if (parameter.length() > 0) {
+                    String pair[] = parameter.split("=");
+                    String parameterName;
+                    try {
+                        parameterName = URLDecoder.decode(pair[0], "UTF-8");
+                    }
+                    catch (UnsupportedEncodingException e) {
+                        throw new Error(e);
+                    }
+                    if (pair.length == 2) {
+                        map.put(parameterName, pair[1]);
+                    }
+                    else {
+                        map.put(parameterName, null);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ js4ms-core/trunk/src/org/js4ms/service/protocol/http/server/handlers/LoggingOutputHandler.java
@@ -0,0 +1,146 @@
+package org.js4ms.service.protocol.http.server.handlers;
+
+import java.io.IOException;
+import java.util.logging.ErrorManager;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.StreamHandler;
+import java.util.logging.XMLFormatter;
+
+import org.js4ms.service.protocol.http.HttpMessageHeaders;
+import org.js4ms.service.protocol.http.HttpStatusCodes;
+import org.js4ms.service.protocol.rest.entity.Entity;
+import org.js4ms.service.protocol.rest.entity.StringEntity;
+import org.js4ms.service.protocol.rest.handler.TransactionHandler;
+import org.js4ms.service.protocol.rest.headers.SimpleMessageHeader;
+import org.js4ms.service.protocol.rest.message.Request;
+import org.js4ms.service.protocol.rest.message.Response;
+import org.js4ms.util.logging.JsonLogFormatter;
+import org.js4ms.util.logging.LogFormatter;
+
+
+/**
+ * An HTTP transaction handler that publishes log records to an open connection.
+ * Used to stream log records to a client in response to an HTTP request.
+ * 
+ * Parameters in the request URI query string are used to control the  output format for the log records.<p>
+ * The 'output' query string parameter is used to specify the output format.
+ * The choices are 'text', 'xml', 'json', 'jsonp'.
+ * The default format is 'text'.
+ * If the output format is 'jsonp', then the URI query string must also include
+ * a 'callback' parameter that gives the name of a function to use in the
+ * the JSONP callback.
+ * 
+ * @see {@link LoggingConfigurationHandler} - provides logging configuration control via HTTP requests.
+ * @author Greg Bumgardner (gbumgard)
+ */
+public class LoggingOutputHandler implements TransactionHandler {
+
+    @Override
+    public boolean handleTransaction(Request request, Response response) throws IOException {
+
+        String outputType = null;
+        String jsonpCallback = null;
+
+        String query = request.getRequestLine().getUri().getQuery();
+        if (query != null && query.length() > 0) {
+            String parameters[] = query.split("[&;]");
+            for (String parameter : parameters) {
+                if (parameter.length() > 0) {
+                    String pair[] = parameter.split("=");
+                     if (pair[0].equalsIgnoreCase("output")) {
+                         if (pair.length == 2) {
+                             outputType = pair[1];
+                         }
+                         else {
+                             response.setStatus(HttpStatusCodes.BadRequest);
+                             response.setEntity(new StringEntity("output parameter value is missing"));
+                             return true;
+                         }
+                     }
+                     else if (pair[0].equalsIgnoreCase("callback")) {
+                         if (pair.length == 2) {
+                             jsonpCallback= pair[1];
+                         }
+                         else {
+                             response.setStatus(HttpStatusCodes.BadRequest);
+                             response.setEntity(new StringEntity("callback parameter value is missing"));
+                             return true;
+                         }
+                     }
+                }
+            }
+        }
+
+        if (outputType == null) {
+            outputType = "text";
+        }
+
+        Formatter formatter;
+        
+        if (outputType.equals("text")) {
+            formatter = new LogFormatter();
+            response.setHeader(new SimpleMessageHeader(Entity.CONTENT_TYPE,"text/plain"));
+        }
+        else if (outputType.equals("xml")) {
+            formatter = new XMLFormatter();
+            response.setHeader(new SimpleMessageHeader(Entity.CONTENT_TYPE,"text/xml"));
+        }
+        else if (outputType.equals("json")) {
+            formatter = new JsonLogFormatter();
+            response.setHeader(new SimpleMessageHeader(Entity.CONTENT_TYPE,"application/json"));
+        }
+        else if (outputType.equals("jsonp")) {
+            if (jsonpCallback == null) {
+                response.setStatus(HttpStatusCodes.BadRequest);
+                response.setEntity(new StringEntity("callback parameter value is missing"));
+                return true;
+            }
+            formatter = new JsonLogFormatter(jsonpCallback);
+            response.setHeader(new SimpleMessageHeader(Entity.CONTENT_TYPE,"application/javascript"));
+        }
+        else {
+            response.setStatus(HttpStatusCodes.BadRequest);
+            response.setEntity(new StringEntity("output parameter value is invalid"));
+            return true;
+        }
+
+        response.setStatus(HttpStatusCodes.OK);
+        response.setHeader(new SimpleMessageHeader(Entity.CONTENT_LENGTH,String.valueOf(Long.MAX_VALUE)));
+        response.setHeader(new SimpleMessageHeader(HttpMessageHeaders.CONNECTION,"close"));
+        response.send();
+
+        final StreamHandler handler = new StreamHandler(response.getConnection().getOutputStream(), formatter) {
+
+            @Override
+            public void close() {
+                // Do nothing here as we don't want the stream handler closing or flushing the connection output stream.
+            }
+
+            @Override
+            public void publish(final LogRecord record) {
+                super.publish(record);
+                // Flush after every record so log messages are sent when they are generated.
+                flush();
+            }
+        };
+
+        handler.setLevel(Level.FINEST);
+
+        handler.setErrorManager(new ErrorManager() {
+            @Override
+            public void error(String msg, Exception ex, int code) {
+                Logger.getLogger("").removeHandler(handler);
+            }
+        });
+
+        Logger.getLogger("").addHandler(handler);
+
+        // Generate a log message so the client will have something to work with.
+        Logger.getLogger("").info("Log started");
+        return true;
+    }
+
+}
