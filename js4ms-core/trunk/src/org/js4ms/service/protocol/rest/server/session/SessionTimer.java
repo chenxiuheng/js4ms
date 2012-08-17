aa4079f440d42a7622f77aa7bcf02c2f925951e9
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-08-17 06:24:06 +0000

    Moved org.js4ms.service.protocol.rest.server to js4ms-core project.

---
+++ js4ms-core/trunk/src/org/js4ms/service/protocol/rest/server/session/SessionTimer.java
@@ -0,0 +1,28 @@
+package org.js4ms.service.protocol.rest.server.session;
+
+import java.util.Timer;
+
+import org.js4ms.util.task.ReschedulableTask;
+
+
+/**
+ * A task used to terminate timed sessions following a period of inactivity.
+ * 
+ *
+ * @author gbumgard
+ */
+public class SessionTimer extends ReschedulableTask {
+
+    final Session session;
+
+    public SessionTimer(final Timer timer, final Session session) {
+        super(timer);
+        this.session = session;
+    }
+
+    @Override
+    public void run() {
+        this.session.terminate();
+    }
+
+}
