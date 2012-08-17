06950b3d840eca07975c5499dbf3a6e1913fb8b3
gbumgard@gmail.com <gbumgard@gmail.com@a83fe11c-4771-2704-5c91-b5affe63316d>
2012-08-17 06:27:19 +0000

    Moved org.js4ms.service.protocol.rtsp to js4ms-core project.

---
+++ js4ms-core/trunk/src/org/js4ms/service/protocol/rtsp/handlers/TransferTimestampHeader.java
@@ -0,0 +1,23 @@
+package org.js4ms.service.protocol.rtsp.handlers;
+
+import java.io.IOException;
+
+import org.js4ms.service.protocol.rest.handler.TransactionHandler;
+import org.js4ms.service.protocol.rest.message.MessageHeader;
+import org.js4ms.service.protocol.rest.message.Request;
+import org.js4ms.service.protocol.rest.message.Response;
+import org.js4ms.service.protocol.rtsp.RtspMessageHeaders;
+
+
+public class TransferTimestampHeader implements TransactionHandler {
+
+    @Override
+    public boolean handleTransaction(Request request, Response response) throws IOException {
+        // Transfer Timestamp header from request to response
+        if (request.containsHeader(RtspMessageHeaders.TIMESTAMP)) {
+            response.setHeader((MessageHeader)request.getHeader(RtspMessageHeaders.TIMESTAMP).clone());
+        }
+        return false;
+    }
+
+}
