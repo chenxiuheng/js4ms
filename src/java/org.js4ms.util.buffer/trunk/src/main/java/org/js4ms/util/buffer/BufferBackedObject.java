c863d59d804966d61fbdc17fd24da078ee1ee790
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-02 19:57:45 +0000

    Moved org.js4ms.util.buffer source into its own project.

---
+++ src/java/org.js4ms.util.buffer/trunk/src/main/java/org/js4ms/util/buffer/BufferBackedObject.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2009-2010 Larkwood Labs Software.
+ * 
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ * 
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.js4ms.util.buffer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.logging.Logger;
+
+import org.js4ms.util.logging.java.LoggableBase;
+
+
+
+/**
+ * @author Gregory Bumgardner
+ */
+public abstract class BufferBackedObject
+                extends LoggableBase {
+
+    final ByteBuffer buffer;
+
+    /**
+     * Creates a slice of the remaining bytes within the buffer, sets the
+     * limit in the slice to the value specified for the size argument,
+     * and advances the input buffer position by the same amount.
+     */
+    public static ByteBuffer consume(final ByteBuffer buffer, final int size) {
+        ByteBuffer slice = buffer.slice();
+        buffer.position(buffer.position() + size);
+        slice.limit(size);
+        return slice;
+    }
+
+    /**
+     * Creates a buffer containing bytes read from an InputStream.
+     * 
+     * @throws IOException
+     */
+    public static ByteBuffer consume(final InputStream is, final int size) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        is.read(buffer.array(), 0, size);
+        return buffer;
+    }
+
+    /**
+     * @param size
+     */
+    public BufferBackedObject(final int size) {
+        this.buffer = ByteBuffer.allocate(size);
+    }
+
+    /**
+     * @param buffer
+     */
+    public BufferBackedObject(final ByteBuffer buffer) {
+        this.buffer = buffer.slice();
+    }
+
+    /**
+     * @param buffer
+     * @param size
+     */
+    public BufferBackedObject(final ByteBuffer buffer, final int size) {
+        this.buffer = consume(buffer, size);
+    }
+
+    /**
+     * @param buffer
+     * @param size
+     * @throws IOException
+     */
+    public BufferBackedObject(final InputStream is, final int size) throws IOException {
+        this.buffer = consume(is, size);
+    }
+
+    /**
+     * Returns a duplicate of the ByteBuffer instance referenced by this object.
+     * 
+     * @return
+     */
+    public final ByteBuffer getBuffer() {
+        return this.buffer.duplicate();
+    }
+
+    /**
+     * Returns the actual ByteBuffer instance referenced by this object.
+     * 
+     * @return
+     */
+    protected final ByteBuffer getBufferInternal() {
+        return this.buffer;
+    }
+
+    /**
+     * @param buffer
+     */
+    public void writeTo(final ByteBuffer buffer) {
+        this.buffer.rewind();
+        buffer.put(this.buffer);
+        this.buffer.rewind();
+    }
+
+    @Override
+    public void log(final Logger logger) {
+        super.log(logger);
+        logState(logger);
+    }
+
+    /**
+     * Logs member variables declared or maintained by this class.
+     * 
+     * @param logger
+     */
+    private void logState(final Logger logger) {
+        logger.info(this.log.msg(": buffer array-offset=" + this.buffer.arrayOffset() +
+                                 ", position=" + this.buffer.position() +
+                                 ", remaining=" + this.buffer.remaining() +
+                                 ", limit=" + this.buffer.limit() +
+                                 ", capacity=" + this.buffer.capacity()));
+    }
+}
---
+++ src/java/org.js4ms.util.buffer/trunk/src/main/java/org/js4ms/util/buffer/fields/ByteField.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2009-2010 Larkwood Labs Software.
+ *
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.js4ms.util.buffer.fields;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+public final class ByteField extends ByteAlignedField<Byte> {
+
+    public ByteField(final int byteOffset) {
+        super(byteOffset);
+    }
+
+    @Override
+    public Byte get(final InputStream is) throws IOException {
+        is.mark(this.offset+1);
+        is.skip(this.offset);
+        int b = (byte)is.read();
+        is.reset();
+        if (b == -1) throw new java.io.EOFException();
+        return (byte)b;
+    }
+
+    @Override
+    public Byte get(final ByteBuffer buffer) {
+        return buffer.get(this.offset);
+    }
+
+    @Override
+    public void set(final ByteBuffer buffer, final Byte value) {
+        buffer.put(this.offset, value);
+    }
+
+}
