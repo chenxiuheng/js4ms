e68711db3be192b2d06502717a28432ad9980915
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-02 19:41:26 +0000

    Moved org.js4ms.service.protocol.rtsp source into its own project.

---
+++ src/java/org.js4ms.service.protocol.rtsp/trunk/src/main/java/org/js4ms/service/protocol/rtsp/TransportPreferences.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2009-2010 Larkwood Labs Software.
+ *
+ * Licensed under the Larkwood Labs Software Source Code License, Version 1.0.
+ * You may not use this file except in compliance with this License.
+ *
+ * You may view the Source Code License at
+ * http://www.larkwoodlabs.com/source-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the license.
+ */
+
+package org.js4ms.service.protocol.rtsp;
+
+import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
+import org.js4ms.service.protocol.rest.RequestException;
+import org.js4ms.util.logging.java.Logging;
+
+
+
+
+/**
+ * Constructs a list of {@link TransportDescription} instances initialized from the transport specifiers 
+ * and parameters contained in an RTSP Transport header.
+ * This list is used to guide stream construction in response to a client setup request.
+ * 
+ * @author Gregory Bumgardner
+ */
+public final class TransportPreferences {
+
+    /*-- Member Variables ----------------------------------------------------*/
+
+    private LinkedList<TransportDescription> descriptions = new LinkedList<TransportDescription>();
+    
+    final String objectId = Logging.identify(this);
+    final String logPrefix = objectId + " ";
+    
+    /*-- Member Functions ----------------------------------------------------*/
+
+    /**
+     * Converts a comma-separated list of transport descriptions from an RTSP Transport header
+     * into a list of {@link TransportDescription} instances
+     * @throws UnknownHostException 
+     * @throws RtspException 
+     */
+    public TransportPreferences(String header) throws RequestException, UnknownHostException {
+        add(header);
+    }
+
+    public void log(Logger logger) {
+        logger.info(this.logPrefix + "----> Transport Preferences");
+        for (TransportDescription description : this.descriptions) {
+            description.log(logger);
+        }
+        logger.info(this.logPrefix + "<---- Transport Preferences");
+    }
+
+    public Iterator<TransportDescription> getIterator() {
+        return this.descriptions.iterator();
+    }
+
+    public void add(String header) throws RequestException, UnknownHostException {
+        String[] descriptions = header.split(",");
+        for (String description : descriptions) {
+            this.descriptions.add(new TransportDescription(description));
+        }
+    }
+}
---
+++ src/java/org.js4ms.service.protocol.rtsp/trunk/src/main/java/org/js4ms/service/protocol/rtsp/handlers/TransferSessionHeader.java
@@ -0,0 +1,28 @@
+package org.js4ms.service.protocol.rtsp.handlers;
+
+import java.io.IOException;
+
+import org.js4ms.service.protocol.rest.handler.TransactionHandler;
+import org.js4ms.service.protocol.rest.message.MessageHeader;
+import org.js4ms.service.protocol.rest.message.Request;
+import org.js4ms.service.protocol.rest.message.Response;
+import org.js4ms.service.protocol.rtsp.RtspMessageHeaders;
+
+
+
+public class TransferSessionHeader implements TransactionHandler {
+
+    public TransferSessionHeader() {
+        
+    }
+
+    @Override
+    public boolean handleTransaction(final Request request, final Response response) throws IOException {
+        // Transfer Session header from request to response
+        if (request.containsHeader(RtspMessageHeaders.SESSION)) {
+            response.setHeader((MessageHeader)request.getHeader(RtspMessageHeaders.SESSION).clone());
+        }
+        return false;
+    }
+
+}
