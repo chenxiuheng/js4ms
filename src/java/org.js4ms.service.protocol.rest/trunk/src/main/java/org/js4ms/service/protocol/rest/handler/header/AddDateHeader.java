5a7f14e62060e3b39b61439593acb277890195a1
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-16 09:28:10 +0000

    Create subpackage under .rest.handler for header-specific handlers, i.e. .rest.handler.header

--- src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/handler/header/AddDateHeader.java
+++ src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/handler/header/AddDateHeader.java
@@ -1,4 +1,4 @@
-package org.js4ms.service.protocol.rest.handlers;
+package org.js4ms.service.protocol.rest.handler.header;
 
 import java.util.Date;
 
--- src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/handler/header/AddServerHeader.java
+++ src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/handler/header/AddServerHeader.java
@@ -1,4 +1,4 @@
-package org.js4ms.service.protocol.rest.server.handlers;
+package org.js4ms.service.protocol.rest.handler.header;
 
 import org.js4ms.service.protocol.rest.MessageHeaders;
 import org.js4ms.service.protocol.rest.handler.ResponseHandler;
