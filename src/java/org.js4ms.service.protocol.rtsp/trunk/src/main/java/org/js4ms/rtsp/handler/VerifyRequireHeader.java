7eeea6e200c559633f8241400dfc31774074d2d7
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-17 23:01:20 +0000

    Renamed .service.rtsp to .rtsp.

---
+++ src/java/org.js4ms.service.protocol.rtsp/trunk/src/main/java/org/js4ms/rtsp/handler/VerifyRequireHeader.java
@@ -0,0 +1,65 @@
+package org.js4ms.service.rtsp.handler;
+
+import java.io.IOException;
+import java.util.HashSet;
+
+import org.js4ms.service.rest.handler.TransactionHandler;
+import org.js4ms.service.rest.header.SimpleMessageHeader;
+import org.js4ms.service.rest.message.HeaderName;
+import org.js4ms.service.rest.message.MessageHeader;
+import org.js4ms.service.rest.message.Request;
+import org.js4ms.service.rest.message.Response;
+import org.js4ms.service.rest.message.StatusCode;
+
+
+
+public class VerifyRequireHeader implements TransactionHandler {
+
+    private HashSet<String> features = null;
+
+    public VerifyRequireHeader() {
+        
+    }
+
+    public void addFeature(final String name) {
+        if (this.features == null) {
+            this.features = new HashSet<String>();
+        }
+        this.features.add(name);
+    }
+
+    @Override
+    public boolean handleTransaction(Request request, Response response) throws IOException {
+        if (request.containsHeader(HeaderName.REQUIRE)) {
+            // Indicate that no special features are supported
+            MessageHeader header = request.getHeader(HeaderName.REQUIRE);
+            if (this.features == null || this.features.isEmpty()) {
+                response.setStatus(StatusCode.OptionNotSupported);
+                response.setHeader(new SimpleMessageHeader(HeaderName.UNSUPPORTED, header.getValue()));
+                return true;
+            }
+            else {
+                // If multiple Require headers were present in the message, they
+                // will have been concatenated into a single comma-delimited list by the parser.
+                // Split the header value into individual feature names and check each.
+                StringBuffer headerValue = new StringBuffer();
+                String[] names = header.getValue().split(",[ ]*");
+                for (String name : names) {
+                    if (!this.features.contains(name)) {
+                        if (headerValue.length() > 0) {
+                            headerValue.append(",");
+                        }
+                        headerValue.append(name);
+                    }
+                }
+                if (headerValue.length() != 0) {
+                    response.setStatus(StatusCode.OptionNotSupported);
+                    response.setHeader(new SimpleMessageHeader(HeaderName.UNSUPPORTED,headerValue.toString()));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/java/org.js4ms.service.protocol.rtsp/trunk/src/main/java/org/js4ms/rtsp/message/RtspHeaderName.java
@@ -0,0 +1,18 @@
+package org.js4ms.service.rtsp.message;
+
+import org.js4ms.service.rest.message.HeaderName;
+
+public interface RtspHeaderName extends HeaderName {
+    public static final String BANDWIDTH = "Bandwidth"; // rtsp(R)
+    public static final String BLOCKSIZE = "Blocksize"; // rtsp(R)
+    public static final String CONFERENCE = "Conference"; // rtsp(R)
+    public static final String CONTENT_BASE = "Content-Base"; // rtsp(e)
+    public static final String PUBLIC = "Public"; // rtsp(r)
+    public static final String RTP_INFO = "RTP-Info"; // rtsp(r)
+    public static final String SCALE = "Scale"; // rtsp(R->r)
+    public static final String SESSION = "Session"; // rtsp(R->r)
+    public static final String SPEED = "Speed"; // rtsp(R->r)
+    public static final String TIMESTAMP = "Timestamp"; // rtsp(R->r)
+    public static final String TRANSPORT = "Transport"; // rtsp(R->r)
+    public static final String X_SESSIONCOOKIE = "x-sessioncookie";
+}
