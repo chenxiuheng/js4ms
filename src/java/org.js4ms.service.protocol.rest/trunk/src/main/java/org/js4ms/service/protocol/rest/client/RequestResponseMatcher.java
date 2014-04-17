a0c309fea1ad2fead08c1aa12d7d3c040c062453
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-17 05:44:15 +0000

    Reorganized rest, http, and rtsp packages and classes.

--- src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/client/RequestResponseMatcher.java
+++
@@ -1,8 +0,0 @@
-package org.js4ms.service.protocol.rest.client;
-
-import org.js4ms.service.protocol.rest.message.Request;
-import org.js4ms.service.protocol.rest.message.Response;
-
-public interface RequestResponseMatcher {
-    public boolean isResponseTo(final Request request, final Response response);
-}
--- src/java/org.js4ms.service.protocol.rest/trunk/src/main/java/org/js4ms/service/protocol/rest/handler/RequestHandlerResolver.java
+++
@@ -1,8 +0,0 @@
-package org.js4ms.service.protocol.rest.handler;
-
-import org.js4ms.service.protocol.rest.common.RequestException;
-import org.js4ms.service.protocol.rest.message.Request;
-
-public interface RequestHandlerResolver {
-    RequestHandler getHandler(Request request) throws RequestException;
-}
