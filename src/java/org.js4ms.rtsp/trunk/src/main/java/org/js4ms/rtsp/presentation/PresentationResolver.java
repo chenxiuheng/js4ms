2380bf8a9511f6e0ea91e4d538deed7091c09790
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-18 09:09:19 +0000

    Moved all of the js4ms JSDK into new Maven project.

--- src/java/org.js4ms.rtsp/trunk/src/main/java/org/js4ms/rtsp/presentation/PresentationResolver.java
+++
@@ -1,8 +0,0 @@
-package org.js4ms.rtsp.presentation;
-
-import org.js4ms.rest.common.RequestException;
-import org.js4ms.rest.message.Request;
-
-public interface PresentationResolver {
-    Presentation getPresentation(Request request) throws RequestException;
-}
