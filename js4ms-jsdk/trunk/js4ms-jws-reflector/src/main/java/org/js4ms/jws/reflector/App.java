06d95847c39478ecae8fa6bf254eb7cb80977941
admin@js4ms.org <admin@js4ms.org@a83fe11c-4771-2704-5c91-b5affe63316d>
2014-04-19 20:11:00 +0000

    Moved JWS reflector into Maven project.

---
+++ js4ms-jsdk/trunk/js4ms-jws-reflector/src/main/java/org/js4ms/jws/reflector/App.java
@@ -0,0 +1,13 @@
+package org.js4ms.jws.reflector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
